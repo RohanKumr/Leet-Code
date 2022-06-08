2e8739335bf82bfc9ca607a5a05cf2789a87ed5e
Rohan Kumar <rkrohanrk065@gmail.com>
2022-06-09 02:16:37 +0530

    Update reverse string.js

--- recursion/reverse string.js
+++ recursion/reverse string.js
@@ -1,20 +1,25 @@
 function reverse (s, i = 0, n= null) {
-		if(i == (s.length - 1) /2){
-    	return s
-    } else {
-    	const end = i == s.length / 2 
-      
-    	let startingIndex =  i;
-      let endingIndex = n || s.length - 1
-			
-      const temp = s[startingIndex];
-			s[startingIndex] = s[endingIndex];
-			s[endingIndex] = temp
-			
-      if (end) return s;
-      return reverse(s, ++startingIndex , --endingIndex )	
-    }
+
+	if ( i == n) return s
+	let startingIndex =  i;
+	let endingIndex = n  || s.length - 1
+
+	const temp = s[startingIndex];
+	s[startingIndex] = s[endingIndex];
+	s[endingIndex] = temp
+
+	if(i == s.length  / 2  ) return s
+	return reverse(s, ++startingIndex , --endingIndex )	
 }
-const input = ['h', 'e', 'l','t','o', 'i']
+/* const input = ['h', 'e', 'l','t','o', 'i','a','b','c','d','e','g'] */
+const input = ['a','b','c','d','e','f']
 const ans = reverse(input);
-console.log(ans)
+/* console.log(ans) */
+
+
+function verify() {
+	const rev = input.reverse();
+	return ans == rev
+}
+
+console.log(verify())
