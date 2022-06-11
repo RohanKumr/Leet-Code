59fe59aaeda92f0a9571afa642b2dcd80efbc236
Rohan Kumar <rkrohanrk065@gmail.com>
2022-06-11 14:12:48 +0530

    Update reverse string.js

--- recursion/reverse string.js
+++ recursion/reverse string.js
@@ -1,16 +1,12 @@
-function reverseString (s, i = 0, n= null) {
-	if(s.length === 1) return s
-	if (  i == n) return s
-    	
-	let startingIndex =  i;
-      	let endingIndex = n  || s.length - 1
-			
-     	const temp = s[startingIndex];
-	s[startingIndex] = s[endingIndex];
-	s[endingIndex] = temp
-      
-	if(i == (s.length / 2) - 1 ) return s
-     	return reverseString(s, ++startingIndex , --endingIndex )	
+function reverseString(s, i = 0) {
+  const ln = s.length
+  if (ln == 1) return s;
+  const last = ln - 1 - i;
+  if (i > last) return s;
+  const temp = s[i];
+  s[i] = s[last];
+  s[last] = temp;
+  return reverseString(s, ++i);
 }
 
 const input = ["A"," ","m","a","n",","," ","a"," ","p","l","a","n",","," ","a"," ","c","a","n","a","l",":"," ","P","a","n","a","m","a"]
